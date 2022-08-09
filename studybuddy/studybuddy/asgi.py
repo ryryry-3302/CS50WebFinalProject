bde7c693f1232c0d3b2560095c7916b16338c9a5
ryryry-3302 <rjleong2002@gmail.com>
2022-08-10 07:53:17 +0800

    made app

--- studybuddy/studybuddy/asgi.py
+++ studybuddy/studybuddy/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for StudyBuddy project.
+ASGI config for studybuddy project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudyBuddy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybuddy.settings')
 
 application = get_asgi_application()
--- studybuddy/studybuddy/urls.py
+++ studybuddy/studybuddy/urls.py
@@ -1,4 +1,4 @@
-"""StudyBuddy URL Configuration
+"""studybuddy URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
