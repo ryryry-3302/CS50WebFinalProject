3e34c765ab3d8ca4ea4962eaa334e96d3515e0e0
ryryry-3302 <rjleong2002@gmail.com>
2022-09-07 09:42:20 +0800

    added daily quotes to homepage

--- studybuddy/studymain/static/studymain/script.js
+++ studybuddy/studymain/static/studymain/script.js
@@ -1,7 +1,32 @@
-console.log("hello")
+console.log("hi")
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementById("backbtn").onclick = function () {
-        location.href = "/login";
+    
+
+    const api_url ="https://quiet-coast-58525.herokuapp.com/https://zenquotes.io/api/quotes/[your_key]";
+
+    async function getapi(url)
+    {
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    var quote = data[0].q
+    var author = data[0].a
+
+    document.getElementById("dailyquote").innerHTML = quote
+    document.getElementById("author").innerHTML = `- ${author}`
     }
+
+    getapi(api_url);
+
+    try {
+        document.getElementById("backbtn").onclick = function () {
+            location.href = "/login";
+        }
+     }
+     catch (e) {
+        console.log(e);
+     }
+
+    
 })
