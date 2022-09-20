0cc3950c7dd904d112fcbf106b481ff477317c7b
ryryry-3302 <rjleong2002@gmail.com>
2022-09-20 10:58:19 +0800

    added form widget

--- studybuddy/studybuddy/settings.py
+++ studybuddy/studybuddy/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -118,7 +119,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- studybuddy/studymain/urls.py
+++ studybuddy/studymain/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from requests import post
+from django.views.i18n import JavaScriptCatalog
 
 from . import views
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register, name="register"),
     path("logout", views.logout_lol, name="logout"),
+    path('jsi18n', JavaScriptCatalog.as_view(), name='js-catlog'),
     path("login", views.login_lol, name="login"),
     path("todolist", views.todolist, name="todo")
 ]
