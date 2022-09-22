624c8925316b7282ced41b7aee151905dc74dadb
ryryry-3302 <rjleong2002@gmail.com>
2022-09-22 17:58:30 +0800

    shit modal

--- studybuddy/studybuddy/settings.py
+++ studybuddy/studybuddy/settings.py
@@ -121,7 +121,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
